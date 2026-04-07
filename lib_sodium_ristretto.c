1984dd363351891d573fce1afa1dad3eaafe8f6e
Tiihott <48@teragrep.com>
2026-04-07 13:34:07 +0300

    Implementing UDF methods. WIP2

--- lib_sodium_ristretto.c
+++ lib_sodium_ristretto.c
@@ -1,6 +1,11 @@
 #include <sodium.h>
 #include <stdio.h>
 #include <string.h>
+#include <mysql/mysql.h>
+
+#ifndef my_bool
+typedef char my_bool;
+#endif
 
 // gcc -Wall -Werror lib_sodium_ristretto.c -lsodium -o lib_sodium_ristretto
 // ./lib_sodium_ristretto
@@ -9,6 +14,20 @@ int main(void)
     if (sodium_init() == -1) {
         return 1;
     }
+
+    char testi[crypto_core_ristretto255_HASHBYTES];
+    unsigned char testipx[crypto_core_ristretto255_BYTES];
+    randombytes_buf(testi, sizeof testi);
+    unsigned char testix = (unsigned char)*testi;
+
+    crypto_core_ristretto255_from_hash(testipx, &testix);
+
+
+
+
+
+
+
 
     unsigned char x[crypto_core_ristretto255_HASHBYTES];
     randombytes_buf(x, sizeof x);
@@ -52,6 +71,7 @@ int main(void)
     free(outstr);
 }
 
+// Initialize the unsigned char px here
 my_bool ristretto_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
     /* make sure user has provided exactly one string argument */
     if (args->arg_count != 1 || (args->arg_type[0] != STRING_RESULT)){
@@ -61,7 +81,7 @@ my_bool ristretto_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
 
     args->maybe_null[0] = 1;
 
-    initid->ptr = malloc( initid->max_length);
+    initid->ptr = malloc(crypto_core_ristretto255_BYTES);
     if (initid->ptr == 0)
     {
         strcpy(message, "ristretto not enough memory for buffer");
@@ -71,11 +91,22 @@ my_bool ristretto_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
     return 0;
 }
 
-
+// Take unsigned char x as input arguments and return the ristretto'ed hash.
 char* ristretto(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error) {
+    if (sodium_init() == -1) {
+        //return 1;
+    }
+    char* x=args->args[0];
+    unsigned char xx, yy;
+    xx = (unsigned char)*x;
+    yy = (unsigned char)*initid->ptr;
 
+    crypto_core_ristretto255_from_hash(&yy, &xx);
+
+    return initid->ptr;
 }
 
+// Free the memory assigned to px.
 void ristretto_deinit(UDF_INIT *initid) {
     if (initid->ptr != 0)
     {
