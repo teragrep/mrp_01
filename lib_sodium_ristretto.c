a81a1d1c18979eb088aca5017f2eab54e8f1c0a2
Tiihott <48@teragrep.com>
2026-04-08 09:59:36 +0300

    Implementing UDF method for crypto_core_ristretto255_from_hash().

--- lib_sodium_ristretto.c
+++ lib_sodium_ristretto.c
@@ -7,71 +7,6 @@
 typedef char my_bool;
 #endif
 
-// gcc -Wall -Werror lib_sodium_ristretto.c -lsodium -o lib_sodium_ristretto
-// ./lib_sodium_ristretto
-int main(void)
-{
-    if (sodium_init() == -1) {
-        return 1;
-    }
-
-    char testi[crypto_core_ristretto255_HASHBYTES];
-    unsigned char testipx[crypto_core_ristretto255_BYTES];
-    randombytes_buf(testi, sizeof testi);
-    unsigned char testix = (unsigned char)*testi;
-
-    crypto_core_ristretto255_from_hash(testipx, &testix);
-
-
-
-
-
-
-
-
-    unsigned char x[crypto_core_ristretto255_HASHBYTES];
-    randombytes_buf(x, sizeof x);
-    unsigned char px[crypto_core_ristretto255_BYTES];
-
-    crypto_core_ristretto255_from_hash(px, x);
-
-    unsigned char r[crypto_core_ristretto255_SCALARBYTES];
-    unsigned char gr[crypto_core_ristretto255_BYTES];
-    unsigned char a[crypto_core_ristretto255_BYTES];
-    crypto_core_ristretto255_scalar_random(r);
-    crypto_scalarmult_ristretto255_base(gr, r);
-    crypto_core_ristretto255_add(a, px, gr);
-
-    unsigned char rr[crypto_core_ristretto255_SCALARBYTES];
-    if (crypto_core_ristretto255_sub(rr, a, px) == -1) {
-        return 1;
-    }
-
-    if (memcmp(rr, gr, crypto_core_ristretto255_BYTES) != 0) {
-        return 1;
-    }
-
-    const size_t arrlen = sizeof(rr);
-    const size_t hexlen = 2; // hex representation of byte with leading zero
-    const size_t outstrlen = arrlen * hexlen;
-
-    char * outstr = malloc(outstrlen + 1);
-    if (!outstr) {
-        fprintf(stderr, "Failed to allocate memory\n");
-        return 1;
-    }
-
-    // Create a string containing a hex representation of `bytearr`
-    char * p = outstr;
-    for (size_t i = 0; i < arrlen; i++) {
-        p += sprintf(p, "%.2x", rr[i]);
-    }
-
-    printf("String variable contains:\n%s\n", outstr);
-    free(outstr);
-}
-
-// Initialize the unsigned char px here
 my_bool ristretto_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
     /* make sure user has provided exactly one string argument */
     if (args->arg_count != 1 || (args->arg_type[0] != STRING_RESULT)){
@@ -106,7 +41,6 @@ char* ristretto(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *l
     return initid->ptr;
 }
 
-// Free the memory assigned to px.
 void ristretto_deinit(UDF_INIT *initid) {
     if (initid->ptr != 0)
     {
