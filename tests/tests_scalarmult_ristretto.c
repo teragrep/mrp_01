03c5b75a9b7d51381aca991261071812aafd78a9
Tiihott <48@teragrep.com>
2026-05-11 15:38:54 +0300

    Added tests_scalarmult_ristretto.c

---
+++ tests/tests_scalarmult_ristretto.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <mysql/mysql.h>
+#include <sodium.h>
+#include <string.h>
+
+#include "../lib_sodium_ristretto.h"
+
+void testScalarmultristretto_init() {
+    unsigned char point[crypto_core_ristretto255_BYTES];
+    crypto_core_ristretto255_random(point);
+    unsigned char scalar[crypto_core_ristretto255_SCALARBYTES];
+    crypto_core_ristretto255_scalar_random(scalar);
+    char *testArgs[] = {scalar, point};
+    unsigned long testLengths[2] = {crypto_core_ristretto255_SCALARBYTES, crypto_core_ristretto255_BYTES};
+    enum Item_result itemValue[2] = {STRING_RESULT, STRING_RESULT};
+    char message[MYSQL_ERRMSG_SIZE];
+    UDF_ARGS args = {.arg_count = 2, .arg_type = itemValue, .args = testArgs, .lengths = testLengths, .maybe_null = 0};
+    UDF_INIT initid = {
+        .maybe_null = 0, .decimals = 3, .max_length = crypto_core_ristretto255_BYTES, .ptr = 0, .const_item = 0
+    };
+    my_bool result = scalarmultristretto_init(&initid, &args, message);
+    assert(result == 0 && "Result is not 0, _init failed when it should have passed.");
+    assert(initid.ptr != 0 && "Memory was not succesfully allocated");
+    free(initid.ptr);
+    printf("testScalarmultristretto_init() passed assertions!\n");
+}
+
+void testInvalidArgsAmountScalarmultristretto_init() {
+    unsigned char scalar[crypto_core_ristretto255_SCALARBYTES];
+    crypto_core_ristretto255_scalar_random(scalar);
+    char *testArgs[] = {scalar};
+    unsigned long testLengths[1] = {crypto_core_ristretto255_SCALARBYTES};
+    enum Item_result itemValue[1] = {STRING_RESULT};
+    char message[MYSQL_ERRMSG_SIZE];
+    UDF_ARGS args = {.arg_count = 1, .arg_type = itemValue, .args = testArgs, .lengths = testLengths, .maybe_null = 0};
+    UDF_INIT initid = {
+        .maybe_null = 0, .decimals = 3, .max_length = crypto_core_ristretto255_BYTES, .ptr = 0, .const_item = 0
+    };
+    my_bool result = scalarmultristretto_init(&initid, &args, message);
+    assert(result == 1 && "Result is not 1, _init passed when it should have failed.");
+    assert(strcmp(message, "requires 2 binary string arguments") == 0 && "Error message is incorrect");
+    assert(initid.ptr == 0 && "Memory was allocated when it shouldn't");
+    printf("testInvalidArgsAmountScalarmultristretto_init() passed assertions!\n");
+}
+
+void testInvalidFirstArgSizeScalarmultristretto_init() {
+    unsigned char point[crypto_core_ristretto255_BYTES];
+    crypto_core_ristretto255_random(point);
+    unsigned char scalar[16];
+    crypto_core_ristretto255_scalar_random(scalar);
+    char *testArgs[] = {scalar, point};
+    unsigned long testLengths[2] = {16, crypto_core_ristretto255_BYTES};
+    enum Item_result itemValue[2] = {STRING_RESULT, STRING_RESULT};
+    char message[MYSQL_ERRMSG_SIZE];
+    UDF_ARGS args = {.arg_count = 2, .arg_type = itemValue, .args = testArgs, .lengths = testLengths, .maybe_null = 0};
+    UDF_INIT initid = {
+        .maybe_null = 0, .decimals = 3, .max_length = crypto_core_ristretto255_BYTES, .ptr = 0, .const_item = 0
+    };
+    my_bool result = scalarmultristretto_init(&initid, &args, message);
+    assert(result == 1 && "Result is not 1, _init passed when it should have failed.");
+    assert(strcmp(message, "First input argument is not a 32 byte scalar in binary string format") == 0 && "Error message is incorrect");
+    assert(initid.ptr == 0 && "Memory was allocated when it shouldn't");
+    printf("testInvalidFirstArgSizeScalarmultristretto_init() passed assertions!\n");
+}
+
+void testInvalidSecondArgSizeScalarmultristretto_init() {
+    unsigned char point[16];
+    size_t i;
+    for (i = 0; i < 16; i++)
+    {
+        point[i] = rand ();
+    }
+    unsigned char scalar[crypto_core_ristretto255_SCALARBYTES];
+    crypto_core_ristretto255_scalar_random(scalar);
+    char *testArgs[] = {scalar, point};
+    unsigned long testLengths[2] = {crypto_core_ristretto255_SCALARBYTES, 16};
+    enum Item_result itemValue[2] = {STRING_RESULT, STRING_RESULT};
+    char message[MYSQL_ERRMSG_SIZE];
+    UDF_ARGS args = {.arg_count = 2, .arg_type = itemValue, .args = testArgs, .lengths = testLengths, .maybe_null = 0};
+    UDF_INIT initid = {
+        .maybe_null = 0, .decimals = 3, .max_length = crypto_core_ristretto255_BYTES, .ptr = 0, .const_item = 0
+    };
+    my_bool result = scalarmultristretto_init(&initid, &args, message);
+    assert(result == 1 && "Result is not 1, _init passed when it should have failed.");
+    assert(strcmp(message, "Second input argument is not a 32 byte binary string") == 0 && "Error message is incorrect");
+    assert(initid.ptr == 0 && "Memory was allocated when it shouldn't");
+    printf("testInvalidSecondArgSizeScalarmultristretto_init() passed assertions!\n");
+}
+
+void testInvalidSecondArgPointScalarmultristretto_init() {
+    unsigned char point[crypto_core_ristretto255_BYTES];
+    size_t i;
+    for (i = 0; i < crypto_core_ristretto255_BYTES; i++)
+    {
+        point[i] = rand ();
+    }
+    unsigned char scalar[crypto_core_ristretto255_SCALARBYTES];
+    crypto_core_ristretto255_scalar_random(scalar);
+    char *testArgs[] = {scalar, point};
+    unsigned long testLengths[2] = {crypto_core_ristretto255_SCALARBYTES, crypto_core_ristretto255_BYTES};
+    enum Item_result itemValue[2] = {STRING_RESULT, STRING_RESULT};
+    char message[MYSQL_ERRMSG_SIZE];
+    UDF_ARGS args = {.arg_count = 2, .arg_type = itemValue, .args = testArgs, .lengths = testLengths, .maybe_null = 0};
+    UDF_INIT initid = {
+        .maybe_null = 0, .decimals = 3, .max_length = crypto_core_ristretto255_BYTES, .ptr = 0, .const_item = 0
+    };
+    my_bool result = scalarmultristretto_init(&initid, &args, message);
+    assert(result == 1 && "Result is not 1, _init passed when it should have failed.");
+    assert(strcmp(message, "Second input argument is not a valid ristretto point") == 0 && "Error message is incorrect");
+    assert(initid.ptr == 0 && "Memory was allocated when it shouldn't");
+    printf("testInvalidSecondArgPointScalarmultristretto_init() passed assertions!\n");
+}
+
+void testScalarmultristretto_deinit() {
+    char *ristrettoPoint = malloc(crypto_core_ristretto255_BYTES);
+    assert(ristrettoPoint != 0);
+    UDF_INIT initid = {.maybe_null=0, .decimals = 3, .max_length = crypto_core_ristretto255_BYTES, .ptr = ristrettoPoint, .const_item = 0};
+    assert(initid.ptr != 0);
+    scalarmultristretto_deinit(&initid);
+    assert(initid.ptr == 0 && "_deinit failed to free the allocated memory.");
+    printf("testScalarmultristretto_deinit() passed assertions!\n");
+}
+
+void testScalarmultristretto() {
+    unsigned char point[crypto_core_ristretto255_BYTES];
+    crypto_core_ristretto255_random(point);
+    unsigned char scalar[crypto_core_ristretto255_SCALARBYTES];
+    crypto_core_ristretto255_scalar_random(scalar);
+    char *testArgs[] = {scalar, point};
+    unsigned long testLengths[2] = {crypto_core_ristretto255_SCALARBYTES, crypto_core_ristretto255_BYTES};
+    enum Item_result itemValue[2] = {STRING_RESULT, STRING_RESULT};
+    UDF_ARGS args = {.arg_count = 2, .arg_type = itemValue, .args = testArgs, .lengths = testLengths, .maybe_null = 0};
+    char *ristrettoPoint = malloc (crypto_core_ristretto255_BYTES);
+    assert(ristrettoPoint != 0);
+    UDF_INIT initid = {
+        .maybe_null = 0, .decimals = 3, .max_length = crypto_core_ristretto255_BYTES, .ptr = ristrettoPoint, .const_item = 0
+    };
+    char result[255];
+    unsigned long length[1];
+    char error[1];
+    char is_null[1];
+    scalarmultristretto(&initid, &args, result, length, is_null, error);
+    assert(crypto_core_ristretto255_is_valid_point(ristrettoPoint) == 1 && "Output of the scalarmultristretto() is not a valid ristretto point");
+    char expecteRistrettoPoint[crypto_core_ristretto255_BYTES];
+    crypto_scalarmult_ristretto255(expecteRistrettoPoint, scalar, point);
+    assert(memcmp(expecteRistrettoPoint, ristrettoPoint, crypto_core_ristretto255_BYTES) == 0 && "Output of the scalarmultristretto() is not as expected");
+    printf("testScalarmultristretto() passed assertions!\n");
+    free(ristrettoPoint);
+}
+
+int main() {
+    testScalarmultristretto_init();
+    testInvalidArgsAmountScalarmultristretto_init();
+    testInvalidFirstArgSizeScalarmultristretto_init();
+    testInvalidSecondArgSizeScalarmultristretto_init();
+    testInvalidSecondArgPointScalarmultristretto_init();
+    testScalarmultristretto_deinit();
+    testScalarmultristretto();
+    return 0;
+}
